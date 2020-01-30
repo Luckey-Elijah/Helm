de96e2ce2a100408a715ee497581fcbd49f2d9e2
Elijah Luckey <luckeyelijah112@gmail.com>
2020-01-30 08:53:34 -0500

    Added LoginView with a pressable button

--- lib/View/Widgets/LoginView.dart
+++ lib/View/Widgets/LoginView.dart
@@ -1,9 +1,54 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 50),
+              _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+Widget _signInButton() {
+  return OutlineButton(
+    splashColor: Colors.grey,
+    onPressed: () {},
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+    highlightElevation: 0,
+    borderSide: BorderSide(color: Colors.grey),
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
