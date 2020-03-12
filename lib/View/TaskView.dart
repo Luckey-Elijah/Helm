e0fb4bb312b5601b856d013074c690a9ec0a4621
Elijah Luckey <luckeyelijah112@gmail.com>
2020-03-11 23:46:37 -0400

    completed cleanupi on TaskView.dart

--- lib/View/TaskView.dart
+++ lib/View/TaskView.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-
-import 'package:senior_design/View/Widgets/Button.dart';
-// import 'package:senior_design/ViewModel/TaskViewModel.dart';
-
-class TaskView extends StatelessWidget {
-  Button button = new Button();
-
-
-import 'package:senior_design/View/TaskListView.dart';
-
+import 'TaskListView.dart';
 import 'Widgets/GlowyButton.dart';
 
 class TaskView extends StatelessWidget {
@@ -19,46 +10,27 @@ class TaskView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-        body: Stack(
-      children: <Widget>[
-        new Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('Assets/TaskBackgroundImage.png'),
-              fit: BoxFit.cover,
+      body: Stack(
+        children: <Widget>[
+          new Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('Assets/TaskBackgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-
-        ),
-        Center(
-          child: Text("Tasks Page"),
-        ),
-        Align(
-            alignment: Alignment(0.5, 0.5), // Changes place of widget
-            child: button.build(context) // Created button in different class
-            )
-      ],
-    ));
-
-
-            ),
-           Align(
-             alignment: Alignment(2, .25), // Changes place of widget
-             child: Column(
-                     mainAxisSize: MainAxisSize.min,
-                     children: [
-                       GlowyButton(buttonText: "Mind",pageToShow: listView),
-                       GlowyButton(buttonText: "Body",pageToShow: listView),
-                       GlowyButton(buttonText: "Life",pageToShow: listView),
-                       GlowyButton(buttonText: "Soul",pageToShow: listView)
-                     ]
-                 )
-             ),
-           ]
-        )
+          Center(
+            child: Text("Tasks Page"),
+          ),
+          Align(
+              alignment: Alignment(0.5, 0.5), // Changes place of widget
+              child:
+                  glowButton.build(context) // Created button in different class
+              )
+        ],
+      ),
     );
-
   }
 }
