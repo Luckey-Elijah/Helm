2f9f95fe739c46820f20186e9114423a7cefe9c6
Elijah Luckey <luckeyelijah112@gmail.com>
2020-03-26 20:15:42 -0400

    Implemented Entry class

--- lib/Model/entry.dart
+++ lib/Model/entry.dart
@@ -1,29 +1,52 @@
 class Entry {
+  // String _id;
   String title;
   String body;
-  DateTime date;
-  // constructor for the Entry class
-  Entry({this.title, this.body, this.date});
+  DateTime dateTime;
+
+  // Constructor for the Entry class
+  Entry({this.title, this.body, this.dateTime});
 
   // named constructor for today's date
-  Entry.today() {
-    date = DateTime.now();
+  Entry.now({this.title, this.body}) {
+    dateTime = DateTime.now();
+  }
+
+  // JSON Serialation //
+
+  // Reads the JSON encoding in the local storage
+  Entry.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        body = json['body'],
+        dateTime = DateTime.parse(json['dateTime']);
+
+  /// Used to writing to JSON
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'body': body,
+
+        // "Writing to" is left in String format not DateTime
+        'dateTime': dateTime,
+      };
+
+  // File I/O //
+
+  // Write JSON to device
+  writeToDevice(Entry entry) {}
+
+  // Read JSON from device
+  Future<Entry> readFromDevice({DateTime dateTime}) async {
+    // access storage
+
+    // match dateTime to dateTime of the JSON
+
+    // intialize the value pairs key-value pairs as String?
+
+    // Map it to this vairable
+    Map<String, dynamic> jsonMap = {};
+
+    Entry entry = Entry.fromJson(jsonMap);
+
+    return entry;
   }
 }
-
-Entry firstEntry = Entry(
-  title: 'My First Entry',
-  date: DateTime.parse('06-24-2019'),
-  body: '''
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac turpis posuere, maximus nulla id, ullamcorper massa. Etiam id pretium enim. Sed eget magna scelerisque, volutpat neque eget, lobortis leo. Sed non est dignissim, suscipit metus a, aliquam neque. Curabitur convallis neque sit amet turpis luctus, sodales facilisis metus iaculis. Curabitur gravida sagittis odio non sagittis. Nunc malesuada varius nisl non finibus. Aliquam vel ultricies erat. Nam non orci nec leo molestie vestibulum. Praesent et porta nunc, eget sagittis nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tortor nulla, pellentesque id felis feugiat, egestas lacinia eros. In id tellus nec erat ultricies consectetur.
-  Phasellus lorem tortor, imperdiet eu ornare cursus, consequat in dolor. Ut erat nisl, ornare sed egestas non, ultricies vel est. Morbi sodales vel diam a viverra. Donec eros augue, dapibus blandit mi ac, cursus euismod ligula. Integer pulvinar, felis eget sodales rhoncus, risus ipsum posuere dui, at commodo quam est at quam. Curabitur eu dapibus enim. Nunc metus sem, fringilla a fermentum eu, cursus et sem. Proin eu neque elementum, elementum lacus at, fermentum leo. Proin eu lacus tincidunt, mollis ligula sed, mattis ante. Duis iaculis libero eu leo accumsan egestas. 
-  ''',
-);
-
-Entry secondEntry = Entry(
-  title: 'My Second Entry',
-  date: DateTime.parse('06-26-2019'),
-  body: '''
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam odio neque, lacinia eget ex non, auctor lacinia nisl. Nulla at tortor velit. Etiam vestibulum felis et maximus laoreet. Praesent porttitor, sem et tempor viverra, odio dolor bibendum nibh, id vulputate magna diam quis lacus. Aliquam erat volutpat. Ut auctor accumsan. 
-  ''',
-);
