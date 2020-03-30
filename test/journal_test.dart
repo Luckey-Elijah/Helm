78747b076e6c4a22200c8b8be66687f97d89f284
Elijah Luckey <luckeyelijah112@gmail.com>
2020-03-30 09:32:23 -0400

    Set up Testing for Journal and entry objects

---
+++ test/journal_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+import '../lib/Model/journal.dart';
+
+/// This source file runs a series of test on the [Journal] object.
+
+// Test 1: Maximum number of entries in a Journal.
+
+// Test 2: Maximum character count in a Entry's body field.
+
+// Test 3: Speed test on writing/reading an Entry.
+
+// Test 4: Speed on reading an Entry.
