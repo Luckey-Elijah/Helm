c8d6a9c7c7782e027458533ed0fb91e0901ff8ff
Elijah Luckey <luckeyelijah112@gmail.com>
2020-05-04 15:06:00 -0400

    formats on view and view models

--- lib/View/TaskListView.dart
+++ lib/View/TaskListView.dart
@@ -1,42 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:senior_design/Model/initial_missions.dart';
-import 'package:senior_design/View/TaskListView.dart';
-import 'package:senior_design/ViewModel/MissionViewModel.dart';
-
-import 'Widgets/MissionButton.dart';
-import 'Widgets/UnclickableButton.dart';
 
 class TaskList extends StatelessWidget {
   String title;
 
   TaskList({this.title});
 
-  MissionViewModel missions = new MissionViewModel();
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.lightGreen,
-        body: ListView.builder(
-
+      backgroundColor: Colors.lightGreen,
+      body: ListView.builder(
         //item.Count:,
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
         physics: const AlwaysScrollableScrollPhysics(),
-
-    itemBuilder: (context,index){
+        itemBuilder: (context, index) {
           // title:,
-
-
-    }
-
-
-
-      )
+        },
+      ),
     );
   }
-
-
 }
--- lib/ViewModel/JournalViewModel.dart
+++ lib/ViewModel/JournalViewModel.dart
@@ -1,19 +1,16 @@
-
-import 'package:senior_design/Model/JournalEntry.dart';
 import 'package:senior_design/Model/entry.dart';
 import 'package:senior_design/Model/journal.dart';
 
-class JournalViewModel
-{
-  Journal helmJournal;
-  JournalViewModel(){
-    helmJournal = new Journal();
-  }
+// class JournalViewModel
+// {
+//   Journal helmJournal;
+//   JournalViewModel(){
+//     helmJournal = new Journal();
+//   }
 
-
-  void addEntry(String title, String body )
-  {
-    Entry entry = Entry(body: body, title: title, dateTime: DateTime.now());
-    helmJournal.addEntry(entry);
-  }
-}
+//   void addEntry(String title, String body )
+//   {
+//     Entry entry = Entry(body: body, title: title, dateTime: DateTime.now());
+//     helmJournal.addEntry(entry);
+//   }
+// }
